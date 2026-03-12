fb01ad2785dbba2e40237fec9ee3c77c42d44d5e
NameHere0 <flip.from.2013@gmail.com>
2026-03-12 13:16:54 +0000

    Change background color to 00FF00

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ interface Todo {
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [color, setColor] = useState<string>("#00FF00");
+
+
 
   const addTodo = (text: string) => {
     const newTodo: Todo = {
@@ -23,13 +26,30 @@ function App() {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  // const changeColor = () => {
+  //   setColor()
+  // }
+
   return (
-    <div className="app">
+    <div className="app" style={{backgroundColor: color}}>
       <div className="container">
         <h1>Todo List</h1>
         <TodoInput onAdd={addTodo} />
         <TodoList todos={todos} onDelete={deleteTodo} />
       </div>
+      <div className="color">
+        {/* Apply the state variable directly to the style attribute */}
+        <h1>COLOR CHANGE</h1>
+
+        <style></style>
+        
+        <input 
+          value={color} 
+          onChange={e => setColor(e.target.value)} 
+          placeholder="Type a color (e.g. red, #000)"
+        />
+      </div>
+
     </div>
   );
 }
